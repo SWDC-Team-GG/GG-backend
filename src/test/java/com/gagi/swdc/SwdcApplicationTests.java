f1fe33e03512db66c40f6a4922e5667ac3377ff8
anys34 <anyeseong34@gmail.com>
2023-10-30 00:01:08 +0900

    ADD :: 환경 세팅

---
+++ src/test/java/com/gagi/swdc/SwdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gagi.swdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
