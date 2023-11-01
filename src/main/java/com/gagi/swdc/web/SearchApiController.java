e1385965f35d3bf00fc57c15ea623dfda8e38f80
anys34 <anyeseong34@gmail.com>
2023-11-02 01:49:41 +0900

    FIX :: cors

--- src/main/java/com/gagi/swdc/web/SearchApiController.java
+++ src/main/java/com/gagi/swdc/web/SearchApiController.java
@@ -2,10 +2,14 @@ package com.gagi.swdc.web;
 
 import com.gagi.swdc.service.SearchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class SearchApiController {
     private final SearchService searchService;
+
+//    @PostMapping("/search")
+//    public void
 }
